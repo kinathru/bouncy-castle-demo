ec9f29747cee4a7634a97111c88e46370b2448eb
kinath <kinath.ru@gmail.com>
2018-06-30 11:57:00 +0530

    signing and encryption

---
+++ src/main/java/com/kinath/mis/publickey/ECCEncryption.java
@@ -0,0 +1,58 @@
+package com.kinath.mis.publickey;/*
+Kasun De Zoysa @ UCSC
+Compile:
+javac -cp ../BC/bcprov-jdk16-146.jar ECCEncryption.java 
+Execute:
+java -cp ../BC/bcprov-jdk16-146.jar:. ECCEncryption
+*/
+
+import java.security.Security;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import java.util.Formatter;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class ECCEncryption {
+
+  public static void main(String[]    args) throws Exception    {
+
+  Security.addProvider(new BouncyCastleProvider());
+
+  String        input = "Hello Kasun ..";
+  Cipher cipher = Cipher.getInstance("EC/NONE/PKCS1Padding");
+
+
+  // create the keys
+  KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
+  generator.initialize(1024,new SecureRandom());
+
+
+  KeyPair          pair = generator.generateKeyPair();
+  Key              pubKey = pair.getPublic();
+  Key              privKey = pair.getPrivate();
+
+  // encryption step
+  cipher.init(Cipher.ENCRYPT_MODE,pubKey);
+  byte[] cipherText = cipher.doFinal(input.getBytes());
+  System.out.println("Cipher : " +byteArray2Hex(cipherText));
+  
+  // decryption step
+  cipher.init(Cipher.DECRYPT_MODE,privKey);
+  byte[] plainText = cipher.doFinal(cipherText);
+  System.out.println("Plain : " +new String(plainText));
+
+ }
+
+ private static String byteArray2Hex(byte[] hash) {
+     Formatter formatter = new Formatter();
+     for (byte b : hash) formatter.format("%02x", b);
+     return formatter.toString();
+  }
+
+}
+
