3ede467e451ea7bf7153f5ea835b177b21f7d232
kinath <kinath.ru@gmail.com>
2018-06-30 11:46:10 +0530

    hmac testing

--- src/main/java/com/kinath/mis/hash/HMACExample.java
+++ src/main/java/com/kinath/mis/hash/HMACExample.java
@@ -1,6 +1,7 @@
 package com.kinath.mis.hash;/*
 Kasun De Zoysa @ UCSC
 */
+
 import java.security.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
@@ -9,61 +10,68 @@ import java.io.*;
 import java.util.*;
 
 
-public class HMACExample{
+public class HMACExample
+{
+    public static void main( String args[] )
+    {
+
+        try
+        {
+
+            String key1 = "MIS@UCSC";
+            SecretKeySpec sk1 = new SecretKeySpec( key1.getBytes(), "HMACSHA256" );
+
+            // Create a MAC object using HMAC-MD5 and initialize with key
+            Mac mac1 = Mac.getInstance( "HMACSHA256" );
+            mac1.init( sk1 );
 
 
+            String data1 = "Hello kasun.";
 
-public static void main(String args[]) {
+            byte[] digest1 = mac1.doFinal( data1.getBytes() );
 
- try {
+            System.out.println( "Orig. HMAC: " );
+            for( byte b : digest1 )
+            {
+                System.out.format( "%02x", b );
+            }
+            System.out.println();
 
-    String key1 = "MIS@UCSC";
-    SecretKeySpec sk1 = new SecretKeySpec(key1.getBytes(), "HMACSHA256");
+            //Verification
+            String key2 = "MIS@UCSC";
+            SecretKeySpec sk2 = new SecretKeySpec( key2.getBytes(), "HMACSHA256" );
 
-    // Create a MAC object using HMAC-MD5 and initialize with key
-    Mac mac1 = Mac.getInstance("HMACSHA256");
-    mac1.init(sk1);
+            Mac mac2 = Mac.getInstance( "HMACSHA256" );
+            mac2.init( sk2 );
 
+            String data2 = "Hello kasun.";
 
-    String data1 = "Hello kasun.";
-
-    byte[] digest1 = mac1.doFinal(data1.getBytes());
-
-    System.out.println("Orig. HMAC: ");
-    for(byte b:digest1) System.out.format("%02x",b);
-    System.out.println("");
-
-    //Verification
-    String key2 = "MIS@UCSC";
-    SecretKeySpec sk2 = new SecretKeySpec(key2.getBytes(), "HMACSHA256");
-
-    Mac mac2 = Mac.getInstance("HMACSHA256");
-    mac2.init(sk2);
-    
-    String data2 = "Hello kasun.";
-
-    byte digest2[] = mac2.doFinal(data2.getBytes());
+            byte digest2[] = mac2.doFinal( data2.getBytes() );
 
 
-    System.out.println("New HMAC: ");
-    for(byte b:digest2) System.out.format("%02x",b);
-    System.out.println("");
+            System.out.println( "New HMAC: " );
+            for( byte b : digest2 )
+            {
+                System.out.format( "%02x", b );
+            }
+            System.out.println();
 
-    if(Arrays.equals(digest1,digest2)) 
-     System.out.println("Digest Verified");
+            if( Arrays.equals( digest1, digest2 ) )
+            {
+                System.out.println( "Digest Verified" );
+            }
 
-    else  
-     System.out.println("Digest Verification failed!");
+            else
+            {
+                System.out.println( "Digest Verification failed!" );
+            }
+        }
+        catch( Exception e )
+        {
+            System.out.println( e );
+        }
 
-
-
-  } catch (Exception e) {
-
-     System.out.println(e);
-
-  }
-
- }
+    }
 
 }
 
