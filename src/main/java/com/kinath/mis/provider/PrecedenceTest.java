8812edeb9a427e0c71379986ea750d6f4ca62685
kinath <kinath.ru@gmail.com>
2018-06-30 10:21:02 +0530

    providers testing

--- src/main/java/com/kinath/mis/provider/PrecedenceTest.java
+++ src/main/java/com/kinath/mis/provider/PrecedenceTest.java
@@ -6,16 +6,17 @@ Execute:
 java -cp ../BC/bcprov-jdk16-146.jar:. PrecedenceTest
 */
 
-import javax.crypto.Cipher;
-import java.security.Provider;
-import java.security.Security;
-import javax.crypto.*;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-public class PrecedenceTest {
-   public static void main(String[]    args)  throws Exception  {
-	Security.addProvider(new BouncyCastleProvider());
-        Cipher  cipher = Cipher.getInstance("AES/ECB/NoPadding");
-        System.out.println(cipher.getProvider());
+import javax.crypto.Cipher;
+import java.security.Security;
+
+public class PrecedenceTest
+{
+    public static void main( String[] args ) throws Exception
+    {
+        Security.addProvider( new BouncyCastleProvider() );
+        Cipher cipher = Cipher.getInstance( "AES/ECB/NoPadding" );
+        System.out.println( cipher.getProvider() );
     }
 }
--- src/main/java/com/kinath/mis/provider/UCSCProvider.java
+++ src/main/java/com/kinath/mis/provider/UCSCProvider.java
@@ -3,27 +3,34 @@ package com.kinath.mis.provider;/* UCSC Provider
 */
 
 import java.security.*;
-public class UCSCProvider extends Provider {
 
-    public UCSCProvider() {
+public class UCSCProvider extends Provider
+{
 
-	super("UCSC", 1.0, "UCSC Security Provider v1.0");
-	put("Signature.SHA-1/UCSC", "UCSCSignature");
-	put("KeyPairGenerator.XYZ", "UCSCKeyPairGenerator");
-	put("MessageDigest.XOR", "XORMessageDigest");
-	put("MessageDigest.SHA-1", "UCSCMessageDigestSHA");
-	put("Alg.Alias.MessageDigest.SHA", "SHA-1");
-	put("Alg.Alias.MessageDigest.XYZ", "XOR");
+    public UCSCProvider()
+    {
+
+        super( "UCSC", 1.0, "UCSC Security Provider v1.0" );
+        put( "Signature.SHA-1/UCSC", "UCSCSignature" );
+        put( "KeyPairGenerator.XYZ", "UCSCKeyPairGenerator" );
+        put( "MessageDigest.XOR", "XORMessageDigest" );
+        put( "MessageDigest.SHA-1", "UCSCMessageDigestSHA" );
+        put( "Alg.Alias.MessageDigest.SHA", "SHA-1" );
+        put( "Alg.Alias.MessageDigest.XYZ", "XOR" );
     }
 
-    public static void main(String args[]) {
-	try {
-	    Security.addProvider(new UCSCProvider());
-	    System.out.println("done");
-	    Provider provider = Security.getProvider("UCSC");
-	    System.out.println(provider.getInfo());
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+    public static void main( String args[] )
+    {
+        try
+        {
+            Security.addProvider( new UCSCProvider() );
+            System.out.println( "done" );
+            Provider provider = Security.getProvider( "UCSC" );
+            System.out.println( provider.getInfo() );
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
     }
 }
